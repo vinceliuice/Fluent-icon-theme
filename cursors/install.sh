4b23e2cdd30dad05908a963648c14b57f8e4f4ec
Vince <vinceliuice@hotmail.com>
2021-04-01 09:51:54 +0800

    Add cursors theme

---
+++ cursors/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ROOT_UID=0
+DEST_DIR=
+
+# Destination directory
+if [ "$UID" -eq "$ROOT_UID" ]; then
+  DEST_DIR="/usr/share/icons"
+else
+  DEST_DIR="$HOME/.local/share/icons"
+fi
+
+if [ -d "$DEST_DIR/Fluent-cursors" ]; then
+  rm -r "$DEST_DIR/Fluent-cursors"
+fi
+
+if [ -d "$DEST_DIR/Fluent-dark-cursors" ]; then
+  rm -r "$DEST_DIR/Fluent-dark-cursors"
+fi
+
+cp -r dist $DEST_DIR/Fluent-cursors
+cp -r dist-dark $DEST_DIR/Fluent-dark-cursors
+
+echo "Finished..."
+
